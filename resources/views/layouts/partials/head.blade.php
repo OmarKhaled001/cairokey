e800c37f5f16b59d711b062c398d3e9970380d96
Omar Khaled <96246719+OmarKhaled001@users.noreply.github.com>
2026-03-24 00:14:06 +0200

    Update head.blade.php

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -56,3 +56,20 @@
 
 {{-- Page-specific styles --}}
 @stack('styles')
+
+
+<!-- Snap Pixel Code -->
+<script type='text/javascript'>
+(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+{a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+r.src=n;var u=t.getElementsByTagName(s)[0];
+u.parentNode.insertBefore(r,u);})(window,document,
+'https://sc-static.net/scevent.min.js');
+
+snaptr('init', 'aa12dd56-0e86-48dd-9efc-e02314a5401b', {});
+
+snaptr('track', 'PAGE_VIEW');
+
+</script>
+<!-- End Snap Pixel Code -->
